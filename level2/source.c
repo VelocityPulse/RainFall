171d3f335273eca08cabef6dffa7416f18711d5b
Velocitypulse <velocitypulse@outlook.fr>
2019-10-01 16:16:07 +0200

    [LEVEL2] Reading old eip (32bit only)

--- level2/source.c
+++ level2/source.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
 
+int main();
+
 void p()
 {
 	char str[64]; // (0x4C - 0xC = 0x40 = 64)
 
 	fflush(stdin);
 
-	gets(str);
+//	gets(str);
 
-	if ((eax & 0xb0000000) == 0xb0000000) 
-	{
-		// printf
-		// exit
+	int ebp;
+	//	asm volatile("1: lea 1b(%%rbp), %0;": "=a"(ebp));
+
+	int *regVal;
+	asm("movl %%ebp, %0" : "=r"(regVal) :);
+
+	//	register int *e asm("rbp");
+
+	int e;
+	//	asm("movl %%rbp,%0" : "=r"(e));
+	//	asm("movl %%esp, %0" : "=r" (e));
+
+	printf("volatile ebp     : %#x\n", *regVal);
+	printf("volatipe ebp+0x4 : %#x\n", regVal[1]);
+	printf("function address : %#x\n", main);
+
+
+	/*	if ((eax & 0xb0000000) == 0xb0000000)
+		{
+	// printf
+	// exit
 	}
 	else
 	{
-		// puts
-		// strdup
+	// puts
+	// strdup
 	}
-
+	*/
 }
 
 int main()
