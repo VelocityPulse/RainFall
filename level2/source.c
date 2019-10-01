c40449c1a8b12e75e8ff56173002227d8795efbb
Velocitypulse <velocitypulse@outlook.fr>
2019-10-01 11:11:33 +0200

    [LEVEL2] Update source

--- level2/source.c
+++ level2/source.c
@@ -2,10 +2,23 @@
 
 void p()
 {
-	char str[0x40];
+	char str[64]; // (0x4C - 0xC = 0x40 = 64)
 
 	fflush(stdin);
-	system("/bin/sh");
+
+	gets(str);
+
+	if ((eax & 0xb0000000) == 0xb0000000) 
+	{
+		// printf
+		// exit
+	}
+	else
+	{
+		// puts
+		// strdup
+	}
+
 }
 
 int main()
