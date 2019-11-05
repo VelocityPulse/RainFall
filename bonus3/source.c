02014b4ad96b60bd7eaa245cea09f589b687c7a8
Velocitypulse <velocitypulse@outlook.fr>
2019-11-05 10:40:20 +0100

    [BONUS3] Update source

--- bonus3/source.c
+++ bonus3/source.c
@@ -6,34 +6,29 @@
 int main(int argc, char **argv)
 {
 	FILE* fd;
-	char a[66];
-	int value;
-// space ?
-	char b[33];
+	char first_read[132];
 
 	fd = fopen("/home/user/end/.pass", "r");
 
-	memset(b, 0, 33);
+	memset(first_read, 0, 132);
 
 	if (fd == 0 || argc != 2)
 		return -1;
-	
-	fread(b, 1, 66, fd);
-	value = 0;
 
-	b[atoi(argv[1])] = 0;
+	fread(first_read, 1, 66, fd);
+	first_read[65] = 0;
 
-	fread(a, 1, 65, fd);
+	first_read[atoi(argv[1])] = 0;
+
+	fread(first_read + 66, 1, 65, fd);
 
 	fclose(fd);
 
-	if (strcmp(b, argv[1]) == 0) {
+	if (strcmp(first_read, argv[1]) == 0) {
 		execl("/bin/sh", "sh", 0);
 	} else {
-		puts(a);
+		puts(first_read + 66);
 	}
 
-
-
-return 0;
+	return 0;
 }
